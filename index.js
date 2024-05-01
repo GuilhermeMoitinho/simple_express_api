9a21e4a8451d222c020d3380562463015df373c3
GuilhermeMoitinho <guilhermemoitinho3165@gmail.com>
2024-05-01 18:12:50 -0300

    add async await

--- index.js
+++ index.js
@@ -6,37 +6,37 @@ const app = express();
 
 app.use(express.json());
 
-app.post('/books', (req, res) => 
+app.post('/books', async (req, res) => 
 {
     const {id, title, author, publishedAt} = req.body
 
     const book = {id, title, author, publishedAt}
 
-    books.push(book);
+    await books.push(book);
 
     return res.status(201).json(book);
 
 })
 
-app.get('/books', (req, res) => 
+app.get('/books', async (req, res) => 
 {
     const allBooks = books;
     return res.status(200).json(allBooks);
 })
 
-app.get('/books/:book_id', (req, res) => {
+app.get('/books/:book_id', async (req, res) => {
     const { book_id } = req.params;
-    const bookEspecifico = books.find(book => book.id === book_id);
+    const bookEspecifico = await books.find(book => book.id === book_id);
 
     if(!bookEspecifico) return res.status(404).json('Não foi encontrado')
 
     return res.status(200).json(bookEspecifico);
 });
 
-app.delete('/books/:book_id', (req, res) => 
+app.delete('/books/:book_id', async (req, res) => 
 {
     const { book_id } = req.params;
-    const indexToRemove = books.findIndex(book => book.id === book_id);
+    const indexToRemove = await books.findIndex(book => book.id === book_id);
 
 if (indexToRemove !== -1) {
     books.splice(indexToRemove, 1);
@@ -44,10 +44,10 @@ if (indexToRemove !== -1) {
     return res.status(204).json('deletado')
 })
 
-app.put('/books/:book_id', (req, res) => 
+app.put('/books/:book_id', async (req, res) => 
 {
     const { book_id } = req.params;
-    const bookEspecifico = books.find(book => book.id === book_id);
+    const bookEspecifico = await books.find(book => book.id === book_id);
     const {id, title, author, publishedAt} = req.body
 
     bookEspecifico.id = id;
